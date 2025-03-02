e4a6dabe0d156d14136e8b52efc588bcefdffe42
lucasarakaki <takeshioficial01@gmail.com>
2025-03-02 13:38:21 -0300

    style: code formatting with pint

--- rector.php
+++ rector.php
@@ -1,16 +1,16 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types = 1);
 
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/bootstrap/app.php',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     ->withPreparedSets(
         deadCode: true,
